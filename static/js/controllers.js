a25f160c581387cedb73474ad0d514a88f95067a
Neville Jos <josneville@yahoo.com>
2015-01-09 22:15:12 -0600

    multiple size boxes

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -6,6 +6,16 @@ var controller = function($scope, $window, api) {
 		.error(function(data, status, headers, config) {
 			$window.alert("Oh Nyo");
 		});
+
+	$scope.boxType = function(index) {
+		return (index % 7 == 1) || (index % 7 == 4) || (index % 7 == 5);
+	}
+	$scope.hidingAlgo = function(index) {
+		//var evenMultiple = (Math.floor(index / 7) % 2) == 0;
+		var leftSide = (index % 7 == 1) || (index % 7 == 5);
+		return leftSide;
+		//return evenMultiple && leftSide;
+	}
 } 
 
 module.exports = controller;
