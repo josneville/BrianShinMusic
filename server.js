283e18ba1afc9a7f6b0cf6b8d99a474bdb0dfdf2
Neville Jos <josneville@yahoo.com>
2015-01-20 17:12:08 -0600

    caching started

--- server.js
+++ server.js
@@ -17,12 +17,14 @@ var oauth = {
 
 var blog = new tumblr.Blog('musicbrianshin.tumblr.com', oauth);
 
-app.get('/data', function(req, res) {
-	blog.posts([], function(err, result1) {
-		if (err) {
-			res.status(400).send("Unknown error");
-			return;
-		}
+var data = {}
+
+setInterval(function(){
+  blog.posts([], function(err, result1) {
+    if (err) {
+      res.status(400).send("Unknown error");
+      return;
+    }
     blog.video([], function(err, result2){
       if (err) {
         res.status(400).send("Unknown error");
@@ -31,16 +33,27 @@ app.get('/data', function(req, res) {
       var photos = result1.posts;
       var videos = result2.posts;
       var posts = photos.concat(videos);
-      // posts.sort(function(post1, post2) {
-      //   // This is a comparison function that will result in dates being sorted in
-      //   // DESCENDING order.
-      //   if (new Date(post1.date) > new Date(post2.date)) return -1;
-      //   if (new Date(post1.date) < new Date(post2.date)) return 1;
-      //   return 0;
-      // });
-      res.status(200).send(posts);
+      posts.sort(function(post1, post2) {
+        if (new Date(post1.date) > new Date(post2.date)) return -1;
+        if (new Date(post1.date) < new Date(post2.date)) return 1;
+        return 0;
+      });
+      data = posts;
     });
-	});
+  });
+}, 10000);
+
+app.get('/data', function(req, res) {
+  if (!isEmptyObject(data)){
+    res.status(200).send(data);
+  }
+  else{
+    res.status(400).send("No data yet");
+  }
 });
+
+function isEmptyObject(obj) {
+  return !Object.keys(obj).length;
+}
 
 app.listen(app.get('port'));
